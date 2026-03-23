481043b65ee317f299f118a513f8e80f67a1f0a5
patryCCio <patryk.szczerbinski@o2.pl>
2026-03-23 23:40:25 +0100

    Connect mongodb with cloud

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/betterlife");
+        await mongoose.connect(`mongodb+srv://patryccio:${process.env.AUTH_MONGOOSE}@betterlife.4z6hjsv.mongodb.net/betterlife?appName=Betterlife`);
         console.log("MongoDB connected ✅ (baza: betterlife)");
     } catch (err) {
         console.error("MongoDB connection error:", err);
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,58 +1,58 @@
 import { User } from "../models/user.model.js";
 
 export const getUser = async (req, res) => {
-    try {
-        const { uuid } = req.query;
+  try {
+    const { uuid } = req.query;
 
-        if (!uuid) {
-            return res.status(400).json({ message: "Brak uuid" });
-        }
-
-        const user = await User.findOne({ uuid });
-
-        if (!user) {
-            return res.status(404).json({ message: "User nie istnieje" });
-
-        }
-
-        res.status(200).json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Błąd serwera" });
+    if (!uuid) {
+      return res.status(400).json({ message: "Brak uuid" });
     }
+
+    const user = await User.findOne({ uuid });
+
+    // createUser();
+
+    if (!user) {
+      return res.status(404).json({ message: "User nie istnieje" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Błąd serwera" });
+  }
 };
 
-// const createUser = async () => {
+const createUser = async () => {
+  const user = new User({
+    fullname: "Patryk Szczerbiński",
+    nickname: "patryCCio",
+  });
 
-//     const user = new User({
-//         fullname: "Patryk Szczerbiński",
-//         nickname: "patryCCio"
-//     });
-
-//     await user.save();
-// }
+  await user.save();
+};
 
 export const setPoints = async (req, res) => {
-    try {
-        const { uuid, pointsToAdd } = req.body;
+  try {
+    const { uuid, pointsToAdd } = req.body;
 
-        if (!uuid || typeof pointsToAdd !== "number") {
-            return res.status(400).json({ message: "Błędne dane" });
-        }
-
-        const user = await User.findOneAndUpdate(
-            { uuid },
-            { $inc: { points: pointsToAdd } }, // 🔥 kluczowe
-            { new: true } // zwraca zaktualizowany dokument
-        );
-
-        if (!user) {
-            return res.status(404).json({ message: "User nie istnieje" });
-        }
-
-        res.status(200).json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Błąd serwera" });
+    if (!uuid || typeof pointsToAdd !== "number") {
+      return res.status(400).json({ message: "Błędne dane" });
     }
+
+    const user = await User.findOneAndUpdate(
+      { uuid },
+      { $inc: { points: pointsToAdd } }, // 🔥 kluczowe
+      { new: true }, // zwraca zaktualizowany dokument
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User nie istnieje" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Błąd serwera" });
+  }
 };
